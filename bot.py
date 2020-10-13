1f8519660f9143f5528f697a95611497bd176038
kapzy <71800353+kapzy@users.noreply.github.com>
2020-10-13 14:31:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     for file in os.listdir(cwd+"/cogs"):
         if file.endswith(".py") and not file.startswith("_"):
             bot.load_extension(f"cogs.{file[:-3]}")
-            @client.command()
+@client.command()
 @commands.has_permissions(manage_messages = True)
 async def purge(ctx, amount=15):
 	await ctx.channel.purge(limit=amount)
